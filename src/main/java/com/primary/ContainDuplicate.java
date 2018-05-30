d35fa99a22d9e9209e5e22e9c89104e6ecae7d1a
guozhongdong <guozhongdong@bonc.com.cn>
2018-05-30 14:29:31 +0800

    练习更新

---
+++ src/main/java/com/primary/ContainDuplicate.java
@@ -0,0 +1,41 @@
+package com.primary;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author gzd
+ * @create 2018-05-30 13:38
+ * @desc 给定一个整数数组，判断是否存在重复元素。
+ **/
+public class ContainDuplicate {
+
+    public static void main(String[] args){
+        int[] nums = {1,3,4,1};
+        boolean flag = new ContainDuplicate().containsDuplicate(nums);
+        System.out.println(flag);
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        // 暴力解决算法，空间复杂度为O(n)
+        /*for (int i = 0; i < nums.length ; i++) {
+            for (int j = i+1; j < nums.length ; j++) {
+                if (nums[i] == nums[j]){
+                    return true;
+                }
+            }
+
+        }
+        return false;*/
+        if (nums == null || nums.length==0)
+            return false;
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length ; i++) {
+            set.add(nums[i]);
+        }
+        if (set.size() != nums.length)
+            return true;
+        return false;
+
+    }
+}
---
+++ src/main/java/com/primary/SingleNumber.java
@@ -0,0 +1,31 @@
+package com.primary;
+
+/**
+ * @author gzd
+ * @create 2018-05-30 13:54
+ * @desc 只出现一次的数字
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 思路：使用异或实现，0与任何数异或都是 它本身，一个数与它本身异或为0 ；
+ * 延伸：交换两个数的值，不使用第三方变量.
+ **/
+public class SingleNumber {
+    public static void main(String[] args){
+        int[] nums = {2,1,2,3,4,3,4};
+      int s = new SingleNumber().singleNumber(nums);
+        System.out.println(6^5);
+        System.out.println(s);
+    }
+
+    public int singleNumber(int[] nums) {
+
+        // invalid check
+        if (nums.length == 0) {
+            return -1;
+        }
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
