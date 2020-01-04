6aac27191deddf0301e0e3fc6a379ce7051f4763
014925 <guozhongdong>
2020-01-04 16:27:06 +0800

    数组联系2020年01月04日

---
+++ src/main/java/com/sum2019/array/ForSum_18.java
@@ -0,0 +1,99 @@
+package com.sum2019.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author gzd
+ * @date 2020/1/4 下午2:10
+ *
+ * 四数之和
+ *
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+ *
+ * 注意：
+ *
+ * 答案中不可以包含重复的四元组。
+ *
+ * 示例：
+ *
+ * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+ *
+ * 满足要求的四元组集合为：
+ * [
+ *   [-1,  0, 0, 1],
+ *   [-2, -1, 1, 2],
+ *   [-2,  0, 0, 2]
+ * ]
+ *
+ * 思路：暴力解题法，并去重,嵌套循环
+ * 关键字：排序，嵌套，去重
+ *
+ */
+public class ForSum_18 {
+
+    public static void main(String[] args) {
+
+        int[] arrays = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+
+        List<List<Integer>> result = new ForSum_18().fourSum(arrays,target);
+        System.out.println(result);
+    }
+
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4){
+            return result;
+        }
+        // 先排序，保证最小的在左边
+        Arrays.sort(nums);
+        // 开始嵌套遍历
+        int a,b,c,d;
+        int length = nums.length;
+        // a是从最小开始遍历，
+        for ( a = 0; a <= length-4 ; a++) {
+            //确保nums[a]是变了的
+            if (a>0 && nums[a] == nums[a-1]){
+                continue;
+            }
+            int min = nums[a]+nums[a+1]+nums[a+2]+nums[a+3];
+            if (min > target){
+                break;
+            }
+            int max = nums[a]+nums[length-1]+nums[length-2]+nums[length-3];
+            if (max < target){
+                break;
+            }
+            for (b= a+1; b <= length-3; b++) {
+                if (b>a+1 && nums[b] == nums[b-1]){
+                    continue;
+                }
+                c = b+1;
+                d = length-1;
+                while (c<d){
+                    if (nums[a]+nums[b]+nums[c]+nums[d] < target){
+                        c++;
+                    }else if (nums[a]+nums[b]+nums[c]+nums[d] > target){
+                        d--;
+                    }else {
+                        result.add(Arrays.asList(nums[a],nums[b],nums[c],nums[d]));
+                        while (c<d && nums[c+1] == nums[c]){
+                            c++;
+                        }
+                        while (c<d && nums[d-1] == nums[d]){
+                            d--;
+                        }
+                        c++;
+                        d--;
+                    }
+                }
+            }
+        }
+
+
+        return result;
+    }
+}
---
+++ src/main/java/com/sum2019/array/NextPermutation_31.java
@@ -0,0 +1,61 @@
+package com.sum2019.array;
+
+/**
+ * @author gzd
+ * @date 2020/1/4 下午3:18
+ * 下一个排列
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ *
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ *
+ * 必须原地修改，只允许使用额外常数空间。
+ *
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 根据leetcode 官网题解
+ * 1，5，8，4，7，6，5，3，1
+ * 从右侧遍历，记录一个数，刚好比左边的大，然后交换它俩的位置，后面的数字进行反转，反转为最小序列
+ */
+public class NextPermutation_31 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3};
+        new NextPermutation_31().nextPermutation(nums);
+        System.out.println(nums);
+    }
+
+    public void nextPermutation(int[] nums) {
+
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+
+    }
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
