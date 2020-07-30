8fc0914e1bcc5d21ec6589566981e8b3947136a7
guozhongdong <18272986912@163.com>
2020-07-30 21:39:28 +0800

    遍历二叉树总结

---
+++ src/main/java/com/sum2020/traverse/TreeTraverse.java
@@ -0,0 +1,16 @@
+package com.sum2020.traverse;
+
+/**
+ * @author guozhongdong
+ * @date 2020/7/30
+ * 旨在描绘出所有遍历二叉树的常用形式
+ * 1、先序遍历 (迭代)
+ * 2、中序遍历 (迭代)
+ * 3、后序遍历 (迭代)
+ * 4、层序遍历 (迭代)
+ * 5、广度优先
+ * 6、深度优先
+ */
+public class TreeTraverse {
+
+}
